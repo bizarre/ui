2f41abe03217ceb97b2cfddd33003ab0e2c5bffd
Alexander Maxwell Adewole <alex@bizar.re>
2025-05-07 07:31:14 -0700

    chore: vitest setup

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,1 +1,4 @@
 import '@testing-library/jest-dom/vitest'
+import '@formatjs/intl-datetimeformat/polyfill'
+import '@formatjs/intl-datetimeformat/locale-data/en'
+import '@formatjs/intl-datetimeformat/add-all-tz'
